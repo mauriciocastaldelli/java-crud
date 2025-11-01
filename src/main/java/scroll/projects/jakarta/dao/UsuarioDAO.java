ded15cf0ef8a7ca063ae9f12060e9e40880d7145
mauriciocastaldelli <mauriciocquiderolii@gmail.com>
2025-11-01 16:58:43 -0300

    criação do dao, service e model básicos para usuários

---
+++ src/main/java/scroll/projects/jakarta/dao/UsuarioDAO.java
@@ -0,0 +1,38 @@
+package scroll.projects.jakarta.dao;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import scroll.projects.jakarta.model.Usuario;
+
+import java.util.List;
+
+@ApplicationScoped
+public class UsuarioDAO {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public void salvar(Usuario user){
+        em.persist(user);
+    }
+
+    public void atualizar(Usuario user){
+        em.merge(user);
+    }
+
+    public void deletar(Usuario user){
+        Usuario u = this.pesquisar(user.getId());
+        if (u != null){
+            em.remove(u);
+        }
+    }
+
+    public List<Usuario> listar(){
+        return em.createQuery("SELECT u FROM Usuario u", Usuario.class).getResultList();
+    }
+
+    public Usuario pesquisar(Long id){
+        return em.find(Usuario.class, id);
+    }
+}
---
+++ src/main/java/scroll/projects/jakarta/service/UsuarioService.java
@@ -0,0 +1,35 @@
+package scroll.projects.jakarta.service;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import scroll.projects.jakarta.dao.UsuarioDAO;
+import scroll.projects.jakarta.model.Usuario;
+
+import java.util.List;
+
+@ApplicationScoped
+public class UsuarioService {
+
+    @Inject
+    private UsuarioDAO usuarioDAO;
+
+    public void salvar(Usuario user){
+        usuarioDAO.salvar(user);
+    }
+
+    public void atualizar(Usuario user){
+        usuarioDAO.atualizar(user);
+    }
+
+    public void deletar(Usuario user){
+        usuarioDAO.deletar(user);
+    }
+
+    public List<Usuario> listar(){
+        return usuarioDAO.listar();
+    }
+
+    public Usuario pesquisar(Long id){
+        return usuarioDAO.pesquisar(id);
+    }
+}
